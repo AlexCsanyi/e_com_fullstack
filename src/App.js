65cd42325a62af1846c54bd343d49d3321e7f373
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-10 19:29:26 +0000

    added react bootstrap and created first style file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./homepage.component";
+import { Button } from "react-bootstrap";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <HomePage></HomePage>
+      <Button variant="info">Info</Button>
+      <button className="btn btn-danger">Danger</button>
     </div>
   );
 }
