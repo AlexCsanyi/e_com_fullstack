cd13d333321a0009ce573446ca1e0c45c8ca17c5
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-10 07:07:40 +0000

    started with first component - test content added

---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">NOTES</h1>
+          <span className="subtitle"></span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JOURNALS</h1>
+          <span className="subtitle"></span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">DIARIES</h1>
+          <span className="subtitle"></span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">CALENDARS</h1>
+          <span className="subtitle"></span>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">PLANNERS</h1>
+            <span className="subtitle"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
