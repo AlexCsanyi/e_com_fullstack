83ee6a3d6fe1e5ecc4b61434d8154b228a37530a
AlexCsanyi <alex.csanyi@gmail.com>
2020-03-27 19:39:48 +0000

    handling Auth with firebase

--- src/App.js
+++ src/App.js
@@ -9,19 +9,44 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/header.component";
 import Footer from "./components/footer/footer.component";
 import SignInUpPage from "./pages/sign-in-up/sign-in-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInUpPage}></Route>
-      </Switch>
-      <Footer></Footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInUpPage}></Route>
+        </Switch>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
