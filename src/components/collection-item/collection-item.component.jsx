12196ba6fc420c9427758b45dd6250db336b10e0
AlexCsanyi <alex.csanyi@gmail.com>
2020-03-30 12:38:12 +0100

    image zoom, footer alignment, add to cart button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import CustomButton from "../button/button.component";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="col-md collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
+    <div className="image-container">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      ></div>
+    </div>
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">£ {price}.00</span>
     </div>
+    <CustomButton>Add to Cart</CustomButton>
   </div>
 );
 
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -12,57 +12,55 @@ import "./footer.styles.scss";
 const Footer = () => (
   <div className="container-fluid footer">
     <div className="section container">
-      <footer>
-        <div className="row">
-          <div className="col-lg-12 block">
-            <div className="footer-media">
-              <ul>
-                <li>
-                  <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faTwitterSquare}></FontAwesomeIcon>
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faPinterestSquare}></FontAwesomeIcon>
-                </li>
-              </ul>
-            </div>
+      <div className="row">
+        <div className="col-lg-12 block">
+          <div className="footer-media">
+            <ul>
+              <li>
+                <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faTwitterSquare}></FontAwesomeIcon>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faPinterestSquare}></FontAwesomeIcon>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-4 block">
-            <p>
-              Field Journals, <br />
-              3581 Langley Street, Gray Manor, <br />
-              Essex, UK
-            </p>
-          </div>
-
-          <div className="col-lg-4 block">
-            <p>
-              FAQs
-              <br />
-              Return policy
-              <br />
-              Shipping
-            </p>
-          </div>
-
-          <div className="col-lg-4 block">
-            <p>
-              Careers
-              <br />
-              Community
-              <br />
-              Wholesale
-            </p>
-          </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-4 block">
+          <p>
+            Field Journals, <br />
+            3581 Langley Street, Gray Manor, <br />
+            Essex, UK
+          </p>
         </div>
-      </footer>
+
+        <div className="col-lg-4 block">
+          <p>
+            FAQs
+            <br />
+            Return policy
+            <br />
+            Shipping
+          </p>
+        </div>
+
+        <div className="col-lg-4 block">
+          <p>
+            Careers
+            <br />
+            Community
+            <br />
+            Wholesale
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 );
