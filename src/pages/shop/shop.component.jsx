36953f0ce245faf7c537b8dee0ab53393cdfcc4c
AlexCsanyi <alex.csanyi@gmail.com>
2020-04-06 11:41:20 +0100

    finished withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,8 +8,15 @@ import {
 } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import { updateCollection } from "../../redux/shop/shop.actions";
+import withSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionOverviewWithSpinner = withSpinner(CollectionOverview);
+const CollectionPageWithSpinner = withSpinner(CollectionPage);
 
 class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -19,21 +26,27 @@ class ShopPage extends Component {
     collectionRef.onSnapshot(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollection(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div>
         <Route
           exact
           path={`${match.path}`}
-          component={CollectionOverview}
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
         ></Route>
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         ></Route>
       </div>
     );
